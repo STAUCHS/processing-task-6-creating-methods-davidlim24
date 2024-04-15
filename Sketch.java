cff185aa3a9c2620b647026fca2dc6054e7fb965
davidlim24 <david.lim25@ycdsbk12.ca>
2024-04-15 14:28:06 -0400

    wokwok

--- Sketch.java
+++ Sketch.java
@@ -6,7 +6,7 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-    size(800, 800);
+    size(1200, 1200);
   }
 
   /** 
@@ -14,23 +14,38 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(255, 255, 255);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void drawflower(float x, float y) {
-    fill(objColor);
-    translate(x, y);
-    for (int intPetals = 0; intPetals <= 10; intPetals++) {
-      rotate(PI/4);
-      ellipse(0, 0, size * 0.3, size * 3);
+    background(65, 105, 225);
+    for (int x = 0; x <= width; x += 200) {
+        for (int y = 0; y <= height; y += 200) {
+            drawHouse(x, y, 255, 0, 0, 150); 
+        }
     }
-    ellipse(0, 0, size, size);
-  }
+}
+
+public void drawHouse(int intHouseX, int intHouseY, int intRed, int intGreen, int intBlue, int size) {
+    // Drawing code for the house
+    stroke(0);
+    fill(intRed, intGreen, intBlue);
+    rect(intHouseX, intHouseY, size, size - 50);
+
+    // Drawing code for the roof
+    fill(255, 218, 185); 
+    triangle(intHouseX, intHouseY, intHouseX + size/2, intHouseY - size/2, intHouseX + size, intHouseY);
+
+    // Drawing code for the door and handle
+    fill(255, 102, 102); // Change door color
+    rect(intHouseX + size/4, intHouseY + size/3, size/2, size - 100); 
+    fill(0);
+    ellipse(intHouseX + size/2, intHouseY + size - 50, 10, 10); 
+}
 }
   
+
+  
+  
+  
+  
+ 
   /**
    * Description
    * 
